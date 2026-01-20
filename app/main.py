90c4e4e0beb86350c80ff9ad2b6d58a4bbef9e56
Arnavya <arnavyachettri@gmail.com>
2026-01-20 19:50:43 +0530

    updates

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .routes import calc_bp
 
+
 def create_app():
     app = Flask(__name__, template_folder="templates")
     app.register_blueprint(calc_bp)
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
